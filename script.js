e510502fdf6afbf8de9891c5197a8a1e2a9f4b1e
giorgi merkviladze <giorgimerkviladze@macbooks-MacBook-Pro.local>
2023-10-02 12:33:11 +0400

    hey baby

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ const container = document.querySelector(".box-container");
 const taskInput = document.querySelector("#task-input");
 const warningArea = document.querySelector(".input-container");
 const warning = document.createElement("p");
+const editBtn = document.querySelector("editBtn");
+
+let currTask = null;
 
 warning.innerText = "Please write some task";
 warning.style.color = "red";
 
 function addTask() {
-  if (taskInput.value === "") {
-    warningArea.append(warning);
-    return null;
+  if (currTask) {
+    currentTaskText = currTask;
+    currentTaskText.innerText = taskInput.value;
+    currTask = null;
+  } else {
+    if (taskInput.value === "") {
+      warningArea.append(warning);
+      return null;
+    }
+    warning.remove();
+    addTaskBox(styleTask(createTask()));
   }
-  warning.remove()
-  addTaskBox(styleTask(createTask()));
 }
 
 function createTask() {
@@ -21,14 +30,20 @@ function createTask() {
 }
 
 function styleTask(box) {
-  box.innerHTML = `<p>${taskInput.value}</p> <button class="deleteBtn">Delete</button>`;
   box.classList.add("task");
   return box;
 }
 function addTaskBox(box) {
+  box.innerHTML = `<p class="taskTxt">${taskInput.value}</p> <button class="editBtn">Edit</button> <button class="deleteBtn">Delete</button>`;
   container.append(box);
-  box.addEventListener("click", function () {
+
+  box.querySelector(".deleteBtn").addEventListener("click", function () {
+    console.log("clicked");
     box.remove();
   });
   taskInput.value = "";
+  box.querySelector(".editBtn").addEventListener("click", () => {
+    taskInput.value = box.querySelector(".taskTxt").innerText;
+    currTask = box.querySelector(".taskTxt");
+  });
 }
